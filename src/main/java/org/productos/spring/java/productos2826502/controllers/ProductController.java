59408627eef48cc36fc9ed79cd775ffa5af288bf
Your Name <you@example.com>
2024-07-23 13:27:50 -0500

    Entidades y Repositorios sin bd

---
+++ src/main/java/org/productos/spring/java/productos2826502/controllers/ProductController.java
@@ -0,0 +1,49 @@
+package org.productos.spring.java.productos2826502.controllers;
+
+import java.util.List;
+
+import org.productos.spring.java.productos2826502.entities.Product;
+import org.productos.spring.java.productos2826502.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    //Inyectar el servicio
+    private ProductService service = 
+              new ProductService();
+
+    //Primer endpoint
+    //Endpoint: un metodo en un controlador
+
+    @GetMapping("/saludo")
+    public String saludo(){
+        return "Hola ... 2826502";
+    }
+
+    //Segundo endpoint
+    //Get: Obtener informacion SIN ALTERARLA
+    //     (inmutable)
+    @GetMapping("/{idproducto}")
+    public String findProductById(@PathVariable String idproducto) {
+        return "aqui se mostrará el producto cuyo id es:" + idproducto;
+    }
+
+    @GetMapping
+    public List<Product> findAllProducts() {
+        //utilizo el servicio
+        return service.findAllProducts();
+    }
+    
+    
+    
+
+
+}
